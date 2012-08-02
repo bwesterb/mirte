ecf2f244688c3f513d93d3b74c42dd39b75562f0
Bas Westerbaan <bas@westerbaan.name>
2012-08-02 16:50:25 +0200

    get_a_manager: add threadPool_settings
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- src/__init__.py
+++ src/__init__.py
@@ -8,9 +8,12 @@ from mirte.core import Manager
 
 __singleton_manager = None
 
-def get_a_manager():
+def get_a_manager(threadPool_settings=None):
     """ On first call, creates and returns a @mirte.core.Manager.  On
-        subsequent calls, returns the previously created instance """
+        subsequent calls, returns the previously created instance.
+
+        If it is the first call, it will initialize the threadPool
+        with @threadPool_settings. """
     global __singleton_manager
     if __singleton_manager is None:
         def _thread_entry():
@@ -19,6 +22,8 @@ def get_a_manager():
         l = logging.getLogger('mirte.get_a_manager')
         l.info("Creating new instance")
         m = Manager(logging.getLogger('mirte'))
+        if threadPool_settings:
+            m.update_instance('threadPool', threadPool_settings)
         threading.Thread(target=_thread_entry).start()
         m.running_event.wait()
         __singleton_manager = m
